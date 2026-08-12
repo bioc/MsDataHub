356ccfb5d5238ccaef430893e31b025ae547d1fb
Laurent Gatto <lgatto@protonmail.ch>
2026-08-12 11:34:35 +0200

    fix: move data to zenodo

--- tests/testthat/test_MsDataHub.R
+++ tests/testthat/test_MsDataHub.R
@@ -1,3 +1,19 @@
 test_that("MsDataHub() works", {
-    expect_is(MsDataHub(), "data.frame")
+    expect_s3_class(MsDataHub(), "data.frame")
+})
+
+
+test_that("MsDataHub data", {
+    ttls <- MsDataHub()[[1]]
+    ## Failing because they are on MassIVE
+    ttls <- setdiff(ttls, grep("Ai2025", ttls, value = TRUE))
+    ttls <- setdiff(ttls, grep("D19_15um30cm_SC1.mzML", ttls, value = TRUE))
+    ttls <- setdiff(ttls, grep("OR11_20160122_PG_HeLa_CVB3_CT_A.mzML", ttls, value = TRUE))
+    for (ttl in ttls) {
+        message("*** Getting ", ttl)
+        x <- eval(call(ttl))
+        val <- validObject(x)
+        if (!val) message(ttl, " NOT VALID")
+        expect_true(val)
+    }
 })
